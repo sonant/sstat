1101be53a5a5005aff903a015d7ebf1a11fd062c
Денис Богомольцев <bogomoltsev@reksoft.ru>
2019-02-19 12:48:15 +0300

    first commit

---
+++ go.mod
@@ -0,0 +1,11 @@
+module mod
+
+require (
+	github.com/jinzhu/gorm v1.9.2
+	github.com/jinzhu/inflection v0.0.0-20180308033659-04140366298a // indirect
+	github.com/mattn/go-sqlite3 v1.10.0 // indirect
+	github.com/sirupsen/logrus v1.3.0
+	github.com/stretchr/testify v1.3.0 // indirect
+	golang.org/x/crypto v0.0.0-20190211182817-74369b46fc67 // indirect
+	golang.org/x/sys v0.0.0-20190215142949-d0b11bdaac8a // indirect
+)
---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"os"
+	"os/signal"
+	"sync"
+	"time"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	logger   = logrus.New()
+	endpoint string
+	db       gorm.DB
+)
+
+//SystemData sturcture
+type SystemData struct {
+	gorm.Model
+	CPULoad float32
+	MEMFree int32
+}
+
+func init() {
+	logger.SetFormatter(&logrus.JSONFormatter{})
+}
+
+func collectData(ctx context.Context, wg *sync.WaitGroup) {
+	for {
+		select {
+		case <-ctx.Done():
+			logger.Infoln("Exit from collect data....")
+			wg.Done()
+			return
+		default:
+			time.Sleep(time.Second)
+		}
+	}
+}
+
+func main() {
+	exit := make(chan os.Signal, 1)
+	var wg sync.WaitGroup
+	flag.StringVar(&endpoint, "endpoint", "", "Address of remote server, which recive logs.")
+	flag.Parse()
+
+	//prepare database connection
+	db, err := gorm.Open("sqlite3", "./db.db")
+	if err != nil {
+		logger.Errorln(err)
+	}
+	defer db.Close()
+
+	db.AutoMigrate(&SystemData{})
+	ctx, cancel := context.WithCancel(context.Background())
+
+	signal.Notify(exit, os.Interrupt)
+	wg.Add(1)
+	go collectData(ctx, &wg)
+
+	<-exit
+
+	logger.Infoln("Got exit signal. Stoping...")
+	cancel()
+	wg.Wait()
+	logger.Infoln("Buy!")
+	os.Exit(0)
+
+}
